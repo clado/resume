9d8a695e359960e14ffea06180a0ab6d1c2cfb7a
Arie Van Antwerp <arievanantwerp@gmail.com>
2015-01-18 19:12:32 -0600

    added styling and color data to get preliminary experiences to show up

---
+++ colorData.js
@@ -0,0 +1,12 @@
+colorData = {
+	// #00b454 #086ca2 #ff8b00 #ff3900
+	'skills': {
+		'color': '#00b454'
+	},
+	'employment': {
+		'color': '#086ca2'
+	},
+	'education': {
+		'color': '#ff8b00'
+	}
+}
--- resume.js
+++ resume.js
@@ -3,15 +3,14 @@ angular.module('personalWebsite', [])
 	  return {
       restrict: 'A',
       replace: true,
-      scope: {
-        
-      },
+      scope: { },
       //templateUrl: 'templates/timeline.html',
-      template: '<svg width="100%" height="50%" viewBox="0 0 200 200"><line x1="10" y1="50" x2="190" y2="50" stroke="grey" stroke-width="1"/><a timeline-item ng-repeat="item in items"></a></svg>',
+      template: '<svg class="timeline" preserveAspectRatio="none" viewBox="0 0 {{timespan}} 100"><line x1="0" y1="50" x2="{{timespan}}" y2="50" stroke="grey" stroke-width="2" opacity="1"/><a timeline-experience ng-repeat="item in items" experience="item"></a></svg>',
       link: function(scope, element, attrs) {
+        console.log('stuff')
 
-      	maxDate = new Date()
-      	minDate = _.chain(resumeData)
+      	var maxDate = new Date()
+      	var minDate = _.chain(resumeData)
       	.map(function(object){
       		return _.map(object, function(date) {
       			return date.dates
@@ -31,16 +30,38 @@ angular.module('personalWebsite', [])
     			var section = key
         	return _.map(object, function(item) {
             item.section = section
+            if (item.dates.end == 'TODAY') item.dates.end = maxDate
+            item.dates.start = new Date(item.dates.start)
         		return item
         	})
         })
     		.flatten().value()
 
+        scope.timespan = maxDate - minDate
+
         console.log(scope.items)
+
+        console.log(maxDate - minDate)
 
       }
     }
-    .directive('')
 
-})
+  })
+  .directive('timelineExperience', function() {
+    return {
+      restrict: 'A',
+      scope: {
+        experience: '='
+      },
+      //templateUrl: 'templates/experience.html',
+      template: '<rect ng-if="experience.dates.end" x="97" y="50" width="{{xpWidth}}" height="5" fill="{{color}}" opacity=".7" /> ',
+      link: function(scope, element, attrs) {
+        // console.log(scope.experience)
+        if (scope.experience.dates.end) scope.xpWidth = scope.experience.dates.end - scope.experience.dates.start
+        scope.color = colorData[scope.experience.section].color
+        console.log(scope.color)
+
+      }
+    }
+  })
 
